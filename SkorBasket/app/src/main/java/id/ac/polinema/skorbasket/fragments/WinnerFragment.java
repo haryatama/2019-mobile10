1ada9e14a645e3f8d45ddeeb7c41d8bec293d780
haryatama <44763034+haryatama@users.noreply.github.com>
2019-11-08 13:50:48 +0700

    Percobaan View Model Lanjut (Jobsheet 10)

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/WinnerFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/WinnerFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,14 +28,14 @@ public class WinnerFragment extends Fragment {
 	private SharedScore sharedScore;
 	private Integer scoreHome=0;
 	private Integer scoreVisitor=0;
-
-	public WinnerFragment() {
 		// Required empty public constructor
-	}
+
+
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 	}
 
 	@Override
@@ -48,5 +50,44 @@ public class WinnerFragment extends Fragment {
 		super.onViewCreated(view, savedInstanceState);
 		txtWinner = view.findViewById(R.id.txtWinner);
 		btnReset = view.findViewById(R.id.btnReset);
+
+		sharedScore.getScoreVisitor().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreVisitor = score;
+				if (scoreVisitor > scoreHome){
+					sharedScore.setWinner(false);
+				}
+			}
+		});
+
+		sharedScore.getScoreHome().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreHome = score;
+				if (scoreVisitor < scoreHome){
+					sharedScore.setWinner(true);
+				}
+			}
+		});
+
+		sharedScore.getWinner().observe(requireActivity(), new Observer<Boolean>() {
+			@Override
+			public void onChanged(Boolean winner) {
+				if (winner){
+					txtWinner.setText("Home Win");
+				} else{
+					txtWinner.setText("Visitor Win");
+				}
+			}
+		});
+
+		btnReset.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(0);
+				sharedScore.setScoreHome(0);
+			}
+		});
 	}
 }
