0a9467949719a234e71d13a80370eab0cfbaa872
haryatama <44763034+haryatama@users.noreply.github.com>
2019-11-08 11:49:42 +0700

    Percobaan 1 ( Jobsheet 10 )

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
@@ -14,17 +15,28 @@ public class MainActivity extends AppCompatActivity {
 	private ConstraintLayout rootView;
 	private Button btnChangeColor;
 
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate (Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
-		
 		rootView.setBackgroundColor(generateRandomColor());
 
-		// Tambahkan event klik pada tombol
+			// Tambahkan event klik pada tombol
+		btnChangeColor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+			}
+		});
+
 	}
+
+
+
 
 	private int generateRandomColor(){
 		Random rnd = new Random();
